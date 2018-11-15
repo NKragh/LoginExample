b693a804b538b8ac382dc0ec00ad958544fe476e
NKragh <39466838+NKragh@users.noreply.github.com>
2018-11-15 11:21:48 +0100

    Added stuff

---
+++ LoginExample/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginExample
+{
+    class User
+    {
+        private string _username { get; }
+        private string _password { get; }
+
+        public User(string Username, string Password)
+        {
+            _username = Username;
+            _password = Password;
+        }
+    }
+}
---
+++ LoginExample/ViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight.Command;
+
+namespace LoginExample
+{
+    class ViewModel
+    {
+        List<User> userList = new List<User>();
+        private RelayCommand _buttonCommand;
+
+        public ViewModel()
+        {
+            userList.Add(new User("Nikolaj", "Passw123"));
+            userList.Add(new User("Johan", "Passw321"));
+            userList.Add(new User("Jonas", "Passw213"));
+            
+            _buttonCommand = new RelayCommand(CheckLogin());
+        }
+
+        public RelayCommand ButtonCommand
+        {
+            get { return _buttonCommand; }
+            set { _buttonCommand = value; }
+        }
+
+
+        public void CheckLogin()
+        {
+            bool placeholder = false;
+
+            foreach (var t in userList)
+            {
+                if (t == inputUser)
+                {
+                    placeholder = true;
+                }
+            }
+
+            if (placeholder)
+            {
+                
+            }
+        }
+    }
+}
